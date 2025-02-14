16b8838056fcead5bb16b4d7767ec107fdfe7dec
spolovynko <spolovynko@gmail.com>
2025-02-14 13:42:32 +0100

    [UPD] - imports

--- dynamic_data_masking/analyzer/analyzer_engine_builder/__init__.py
+++ dynamic_data_masking/analyzer/analyzer_engine_builder/__init__.py
@@ -1,7 +1,8 @@
-from .analyzer_engine_builder.analyzer_engine_builder import PresidioAnalyzerBuilder
-from .analyzer_engine_builder.recognizer_registry import RegistryRecognizerBuilder
+from .analyzer_engine_builder import PresidioAnalyzerBuilder
 
-from nlp_configuration import nlp_config
-from recognizers import recognizers   
+from .recognizer_registry import RegistryRecognizerBuilder
+
+from .nlp_configuration import nlp_config
+from .recognizers import recognizers   
 
 __all__ = ['PresidioAnalyzerBuilder', 'RegistryRecognizerBuilder', 'nlp_config', 'recognizers']
