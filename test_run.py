44a4f99deb7656264fcf4fde73d7156793644215
Sergiy <spolovynko@gmail.com>
2025-02-28 16:29:34 +0100

    upadte

--- test_run.py
+++ test_run.py
@@ -1,17 +1,37 @@
-from main import analyze_text,anonymize_text, file_process
+from main import analyze_text,anonymize_text, file_process, mask_file
 path = r'C:\Users\spolo\OneDrive\Documents\DOC\Work\TCS\Code\dynamic_data_masking\ddm\pdf_files\sample_sensitive_pdf.pdf'
+output_path = r'C:\Users\spolo\OneDrive\Documents\DOC\Work\TCS\Code\dynamic_data_masking\ddm\pdf_files\sample_sensitive_pdf_redacted.pdf'
 
 if __name__ == "__main__":
-    print('ANONYMIZER RUNS')
-    text, word_coordinates = file_process(file_path=path, language='eng', resolution=300)
-    print('ANALYZER RUNS')
-    
-    result = analyze_text(text=text, from_config_file=False, language='en',use_predefined=False)
+    print('FILE READER RUNS')
+    text, word_coordinates = file_process(
+        file_path=path, 
+        language='eng', 
+        resolution=500
+        )
+
+    print('ANALYZER RUNS')    
+    result = analyze_text(
+        text=text, 
+        from_config_file=False, 
+        language='en', 
+        use_predefined=False
+        )
 
     print('ANONYMIZER RUNS')
-    masked_text = anonymize_text(text=text,analyzer_results=result)
+    masked_text = anonymize_text(
+        text=text, 
+        analyzer_results=result
+        )
     
-    print(text)
-    print(masked_text)
-    print(word_coordinates)
+    print("REDACTOR RUNS")
+    mask_file(
+        input_file_path=path, 
+        extracted_text=text,
+        masked_text=masked_text,
+        words_info=word_coordinates,
+        output_pdf_path=output_path
+          )
+
+
     
